330e41ad8602c43d354e4e238c0fe5bc3b50dbad
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-10-13 16:48:51 -0300

    add password fields on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,46 @@
+require 'digest'
+
 class User < ActiveRecord::Base
 
-	attr_accessible :name, :email
+	attr_accessor :password
+
+	attr_accessible :name, :email, :password
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	validates :name, :presence => true, :length => { :maximum => 50 }
 
 	validates :email, :presence => true, :format =>{ :with => email_regex }, 
-																		:uniqueness =>{:case_sensitive => false}
+																:uniqueness =>{:case_sensitive => false}
+	before_save :encrypt_password
+
+	def has_password?(submitted_password)
+		encrypted_password == encrypt(submitted_password)
+	end
+
+	def authenticate(email, submitted_password)
+
+	end
+
+
+private
+
+	def encrypt_password
+		self.salt = make_salt if new_record?
+		self.encrypted_password = encrypt(password)
+	end
+
+	def encrypt(string)
+		secure_hash("#{salt}--#{string}")
+	end
+
+	def make_salt
+		secure_hash("#{Time.now.utc}--#{password}")
+	end
+	
+	def secure_hash(string)
+		Digest::SHA2.hexdigest(string)
+	end
 
 end
 
@@ -15,10 +48,12 @@ end
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
