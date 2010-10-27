400f023ee72b656a84b5055530b3542b54073785
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-10-27 15:46:22 -0300

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
 	def new
+		@user = User.new
 		@title="Sign up"
-		@user = User.create
 	end
 
 	def show
@@ -13,4 +13,21 @@ class UsersController < ApplicationController
 			@title="#{params[:id]} não existe"
 		end
 	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to user_path @puser, :notice => 'User was successfully created.'
+#	      format.html { redirect_to(@puser, :notice => 'User was successfully created.') }
+#			format.xml  { render :xml => @user, :status => :created, :location => @user }
+		else
+			@title = "Sign up"
+			render 'new'
+#			format.html { render :action => "new" }
+#			format.xml  { render :xml => @patient.errors, :status => :unprocessable_entity }
+
+		end
+	end
+
+
 end
--- spec/views/patients/show.html.erb_spec.rb
+++ spec/views/patients/show.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe "patients/show.html.erb" do
-  before(:each) do
-    @patient = assign(:patient, stub_model(Patient,
-      :name => "Name"
-    ))
-  end
+#describe "patients/show.html.erb" do
+#  before(:each) do
+#    @patient = assign(:patient, stub_model(Patient,
+#      :name => "Name"
+#    ))
+#  end
 
-  it "renders attributes in <p>" do
-    render
-    rendered.should contain("Name".to_s)
-  end
-end
+#  it "renders attributes in <p>" do
+#    render
+#    rendered.should contain("Name".to_s)
+#  end
+#end
