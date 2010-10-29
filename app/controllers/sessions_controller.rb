274404a96bf5d003b11f5ac8c8186bb324d4fca8
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-10-29 16:13:15 -0300

    signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,6 @@ class SessionsController < ApplicationController
 
   def create
 
-	#	render 'new'
-
 		user = User.authenticate(params[:session][:email], params[:session][:password])
 
 		if user.nil?
@@ -16,7 +14,8 @@ class SessionsController < ApplicationController
 			@title = "Sign in"
 			render 'new'
 		else
-		# Sign the user in and redirect to the user's show page.
+			sign_in user
+			redirect_to user
 		end
 
   end
