c5737b482e92681b3564333a0fe0af68b14a8841
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-11-17 14:32:33 -0300

    teste

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+$(function(){
+	if ( $("#microposts").length>0 ){
+		setTimeout(updateMicroposts,5000);
+	}
+});
 
+function updateMicroposts() {  
+	var current_user=$("#microposts").attr("data-current_user");
+	$.getScript("/users/" + current_user + "/microposts.js")
+	setTimeout(updateMicroposts, 5000);  
+}  
